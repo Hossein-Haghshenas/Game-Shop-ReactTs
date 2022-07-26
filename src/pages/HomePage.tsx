83195d0db576429837794735574519a0f075e09b
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-26 19:08:43 +0430

    Display data

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import GameListContainer from "./../components/GameList.container";
+
+const HomePage = (): ReactElement => {
+  return <GameListContainer />;
+};
+
+export default HomePage;
