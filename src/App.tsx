4639d98fa79514b1f737bb407ac379f2453137f5
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-26 12:04:07 +0430

    Set something in app and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
-  return <section></section>;
+  return (
+    <section>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </section>
+  );
 };
 
 export default App;
