3063874b83b42d3bcab1760f98be63bca1634871
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-26 12:03:26 +0430

    Create Game card component

---
+++ src/components/GameCards.tsx
@@ -0,0 +1,44 @@
+import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import { Game } from "../types/Games.types";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+interface Props {
+  content: Game;
+}
+
+const GameCard = ({ content }: Props): ReactElement => {
+  const { id, title, thumbnail, short_description, genre } = content;
+  const link = `/game/${id}`;
+  return (
+    <Link to={link}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={thumbnail}
+            alt={`${title} logo`}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {short_description}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {genre}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
+  );
+};
+
+export default GameCard;
