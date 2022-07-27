04872ae5fd3f43b221c08f10b258dc293300c96f
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-27 19:55:55 +0430

    Fix something

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import "./App.css";
+import GamePage from "./pages/GamePage";
 
 const App = () => {
   return (
     <section>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/games" element={<GamePage />}></Route>
       </Routes>
     </section>
   );
