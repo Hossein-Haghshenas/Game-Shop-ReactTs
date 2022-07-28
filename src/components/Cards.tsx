ad9a693ba80d1bf841ab1802a652c9c5e2fc6574
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-28 18:39:30 +0430

    Create card template

---
+++ src/components/Cards.tsx
@@ -0,0 +1,70 @@
+import { ReactElement } from "react";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Chip,
+  Typography,
+  Box,
+  CardActions,
+  Grid,
+} from "@mui/material";
+import StarRoundedIcon from "@mui/icons-material/StarRounded";
+import CloudDownloadRoundedIcon from "@mui/icons-material/CloudDownloadRounded";
+import { CardProps } from "../types/Card.types";
+
+const Cards = ({ img, title, description, genre }: CardProps): ReactElement => {
+  return (
+    <Card
+      sx={{
+        width: 300,
+        height: "25rem",
+
+        background: `url(${img}) no-repeat`,
+        backgroundSize: "cover",
+        position: "relative",
+      }}
+    >
+      <CardActionArea
+        sx={{
+          borderRadius: "1rem 1rem 0 0",
+          backgroundColor: "#1e283fcf",
+          color: "azure",
+          backdropFilter: "blur(2px)",
+          position: "absolute",
+          bottom: "0",
+        }}
+      >
+        <CardContent sx={{ height: "6rem" }}>
+          <Typography variant="h5" component="h5">
+            {title}
+          </Typography>
+          <Typography variant="h6" component="h6" marginBottom={"1rem"}>
+            {genre}
+          </Typography>
+          <Typography variant="body2">{description}</Typography>
+        </CardContent>
+        <CardActions>
+          <Grid
+            container
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            paddingX={"0.5rem"}
+          >
+            <Chip
+              label={"4.4"}
+              icon={<StarRoundedIcon sx={{ color: "orange !important" }} />}
+              variant="outlined"
+              sx={{ color: "white" }}
+            />
+            <Box>
+              <CloudDownloadRoundedIcon />
+            </Box>
+          </Grid>
+        </CardActions>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default Cards;
