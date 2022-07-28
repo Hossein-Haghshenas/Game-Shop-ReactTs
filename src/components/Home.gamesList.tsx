db5d095cf020703fff7f89e8c37b7bf6c8a86f13
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-28 18:41:50 +0430

    Create home page game list component

---
+++ src/components/Home.gamesList.tsx
@@ -0,0 +1,47 @@
+import { ReactElement } from "react";
+import { Grid } from "@mui/material";
+import Cards from "./Cards";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import LocalFireDepartmentRoundedIcon from "@mui/icons-material/LocalFireDepartmentRounded";
+import { HomeProps } from "../types/HomeGames.types";
+
+const GamesList = ({ homeGames }: HomeProps): ReactElement => {
+  return (
+    <>
+      <Grid container paddingX={{ xs: "1rem", sm: "7rem" }} paddingY={"1rem"}>
+        <Typography
+          variant="h6"
+          marginY={"3rem"}
+          color={"azure"}
+          sx={{ display: "flex", alignItems: "center" }}
+        >
+          <Box
+            sx={{
+              padding: "0.1rem 0.3rem",
+              marginRight: "0.5rem",
+              borderRadius: "50%",
+              bgcolor: "magenta",
+            }}
+          >
+            <LocalFireDepartmentRoundedIcon />
+          </Box>
+
+          {homeGames.gameCategory}
+        </Typography>
+        <Grid container justifyContent={"center"} spacing={3}>
+          {homeGames.games.map((game) => {
+            const { gamesTitle, gamesImg, id } = game;
+            return (
+              <Grid item key={id}>
+                <Cards title={gamesTitle} img={gamesImg} genre={"action"} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default GamesList;
