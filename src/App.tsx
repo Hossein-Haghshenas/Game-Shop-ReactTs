64be7419818d5101500959498873f84a8ace47b4
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-29 22:42:21 +0430

    Import and use in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-import "./App.css";
 import GamePage from "./pages/GamePage";
+import AboutUs from "./pages/AboutUs";
+import Footer from "./components/Footer";
+import "./App.css";
 
 const App = () => {
   return (
-    <section>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/games" element={<GamePage />}></Route>
+        <Route path="/aboutus" element={<AboutUs />}></Route>
       </Routes>
-    </section>
+      <Footer />
+    </>
   );
 };
 
