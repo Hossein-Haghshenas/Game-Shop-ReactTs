a1874bf9ef7f01699e687b58be5d97a1cdd30374
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-29 22:41:52 +0430

    Create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,46 @@
+import { ReactElement } from "react";
+import { Grid } from "@mui/material";
+import { Typography } from "@mui/material";
+
+const Footer = (): ReactElement => {
+  return (
+    <footer>
+      <Grid
+        container
+        justifyContent={"space-around"}
+        alignItems={"center"}
+        marginTop={"1rem"}
+        paddingY={"1rem"}
+        color={"azure"}
+      >
+        <Typography
+          variant="h6"
+          noWrap
+          sx={{
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+          }}
+        >
+          Game
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+            }}
+            color={"magenta"}
+          >
+            Shope
+          </Typography>
+        </Typography>
+        <Typography>Hossein Developer © All Rights Reserved</Typography>
+      </Grid>
+    </footer>
+  );
+};
+
+export default Footer;
