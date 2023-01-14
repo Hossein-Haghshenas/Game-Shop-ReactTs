199f2937e8f06367c5693d791ec511b610cdac6a
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-14 21:10:07 +0330

    Get data with useQuery and create conditional rendering for different situations

--- src/components/GameList.container.tsx
+++ src/components/GameList.container.tsx
@@ -1,17 +1,32 @@
-import { ReactElement, useEffect, useState } from "react";
-import GameListRender from "./GameList.render";
+import { ReactElement } from "react";
+import { useQuery } from 'react-query'
 import { Game } from "../types/Games.types";
-import { getData } from "../api/GameApi";
+import GameListRender from "./GameList.render";
+import { getAllGames } from "../api/GameApi";
+import Loading from "./Loading";
+import Error from "./Error";
+import { Grid } from "@mui/material";
 
 const GameListContainer = (): ReactElement => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [err, setErr] = useState<string>("");
-  useEffect(() => {
-    getData()
-      .then((res) => setGames(res.data))
-      .catch((e) => setErr(e.message));
-  }, []);
-  return <GameListRender err={err} games={games} />;
+  const { data, error, isLoading, isError } = useQuery<Game[], Error>("games", () => getAllGames())
+
+  if (isLoading) {
+    return (
+      <Grid position="absolute" top="40%" container justifyContent="center" alignItems="center">
+        <Loading />
+      </Grid>
+    )
+  }
+  if (isError) {
+    return (
+      <Grid position="absolute" top="35%" container justifyContent="center" alignItems="center">
+        <Error errorMsg={error.message} />
+      </Grid>
+    )
+  }
+  return <>
+    <GameListRender games={data} />
+  </>;
 };
 
 export default GameListContainer;
--- src/components/GameList.render.tsx
+++ src/components/GameList.render.tsx
@@ -1,47 +1,33 @@
-import { CircularProgress, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import { ReactElement } from "react";
 import { Game } from "../types/Games.types";
 import GameCard from "./GameCards";
 
 interface Props {
-  err?: string;
-  games: Game[];
+  games?: Game[];
 }
 
-function GameListRender({ err, games }: Props): ReactElement {
-  err ? (
-    <p>Unable to fetch games</p>
-  ) : games?.length ? (
-    <p>No games available</p>
-  ) : (
-    <></>
-  );
+const GameListRender = ({ games }: Props): ReactElement => {
   return (
     <>
-      {games.length === 0 ? (
-        <Grid container justifyContent={"center"} marginY={"37vh"}>
-          <CircularProgress sx={{ color: "azure" }} />
+      <ul>
+        <Grid
+          container
+          spacing={3}
+          justifyContent={"center"}
+          sx={{ marginTop: "1rem" }}
+        >
+          {games?.map((game) => {
+            return (
+              <Grid item key={game.id}>
+                <li>
+                  <GameCard content={game} />
+                </li>
+              </Grid>
+            );
+          })}
         </Grid>
-      ) : (
-        <ul>
-          <Grid
-            container
-            spacing={3}
-            justifyContent={"center"}
-            sx={{ marginTop: "1rem" }}
-          >
-            {games.map((game) => {
-              return (
-                <Grid item key={game.id}>
-                  <li>
-                    <GameCard content={game} />
-                  </li>
-                </Grid>
-              );
-            })}
-          </Grid>
-        </ul>
-      )}
+      </ul>
     </>
   );
 }
