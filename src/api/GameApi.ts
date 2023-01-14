0928b01e55aaedd527f591741ebdb9d254054329
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-01-14 21:03:36 +0330

    Change game api and Sync that with react query and typescript

--- src/api/GameApi.ts
+++ src/api/GameApi.ts
@@ -1,17 +1,26 @@
 import axios from "axios";
+import { Game } from "../types/Games.types";
 import { API_HOST, API_KEY } from "../constants/api_key";
 
-const options = {
-  method: "GET",
-  url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
-  params: { platform: "pc" },
-  headers: {
-    "X-RapidAPI-Key": API_KEY,
-    "X-RapidAPI-Host": API_HOST,
-  },
+
+export const getAllGames = async (): Promise<Game[]> => {
+  const baseURL: string = "https://free-to-play-games-database.p.rapidapi.com/api/games?pc" ?? ''
+  const { data } = await axios.get(baseURL, {
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": API_HOST,
+    }
+  });
+  return data as Game[];
 };
 
-export const getData = () => {
-  const data = axios.request(options).then((res) => res);
-  return data;
+export const getGameDetails = async (): Promise<Game[]> => {
+  const baseURL: string = `https://free-to-play-games-database.p.rapidapi.com/api/games?pc/id?517` ?? ''
+  const { data } = await axios.get(baseURL, {
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": API_HOST,
+    }
+  });
+  return data as Game[];
 };
